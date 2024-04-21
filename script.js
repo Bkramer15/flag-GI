c3080503b28eb6d98f3ef67c9c017bd64531b490
Birhanu C kramer <Birhanuckramer@AdminisatorsMBP.home>
2024-04-20 22:49:17 -0400

    GI4 flag project

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",  function(){
+
+  document.getElementById('button1').addEventListener('click',redirect);
+  function redirect(){
+    window.location.href="america2.html"
+  }
+})
+
+//button to take user back to classic flag
+document.addEventListener("DOMContentLoaded",  function(){
+
+  document.getElementById('home').addEventListener('click',redirect);
+  function redirect(){
+    window.location.href="index.html"
+  }
+})
